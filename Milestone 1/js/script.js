95b1b4d7d84aae535eeaf35d4afbab8fceac38e7
FR_Jess <fr_jess@icloud.com>
2022-11-07 16:44:06 +0100

    [add] all contacts with Vue

--- Milestone 1/js/script.js
+++ Milestone 1/js/script.js
@@ -1,3 +1,68 @@
-// VARIABLES DECLARATION AND INIZIALIZATION
+// VUE
 
-// FUNCTIONS
+const { createApp } = Vue;
+
+createApp({
+
+  data(){
+    return{
+      contacts: [
+        {
+          name: 'Fabio',
+          avatar: '_1',
+          visible: true,
+          messages:''
+        },
+        {
+          name: 'Gianluca',
+          avatar: '_2',
+          visible: true,
+          messages:''
+        },
+        {
+          name: 'Noah',
+          avatar: '_3',
+          visible: true,
+          messages:''
+        },
+        {
+          name: 'Marco',
+          avatar: '_4',
+          visible: true,
+          messages:''
+        },
+        {
+          name: 'Davide',
+          avatar: '_5',
+          visible: true,
+          messages:''
+        },
+        {
+          name: 'Ilaria',
+          avatar: '_6',
+          visible: true,
+          messages:''
+        },
+        {
+          name: 'Flavio',
+          avatar: '_7',
+          visible: true,
+          messages:''
+        },
+        {
+          name: 'Alessio',
+          avatar: '_8',
+          visible: true,
+          messages:''
+        },
+
+      ]
+    }
+  },
+
+  // methods:{
+
+  // }
+
+}).mount('#app')
+console.log("JS presente")
